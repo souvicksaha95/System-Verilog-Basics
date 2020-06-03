902453480e7eb5474e1d45e9964082ab1aba7bdc
Souvick Saha <souvicksaha95@gmail.com>
2020-06-03 23:53:36 +0530

    Update Array_declaration.sv

--- Array/Array_declaration.sv
+++ Array/Array_declaration.sv
@@ -1,8 +1,9 @@
 module tb;
   bit [7:0]   m_data;
  
-  initial begin
-    m_data = 8'hF1; 
+  initial
+    begin
+      m_data = 8'hF1; 
  
     for (int i = 0; i < $size(m_data); i++) begin
       $display ("m_data[%0d] = %b", i, m_data[i]);
