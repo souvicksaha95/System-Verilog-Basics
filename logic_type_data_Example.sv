25abd635a76372cfd524ffa83236cad3120ddab3
Souvick Saha <souvicksaha95@gmail.com>
2020-06-04 19:17:00 +0530

    Update logic_type_data_Example.sv

--- logic_type_data_Example.sv
+++ logic_type_data_Example.sv
@@ -4,11 +4,11 @@ module new_logic_data_type;
  
   initial 
 	begin
-      		$display ("my_data=0x%0b en=%0b", new_data, en);      // Default value of logic type is X
+		$display ("new_data=0x%0b en=%0b", new_data, en);      // Default value of logic type is X
       		new_data = 4'hB;                   		      // logic datatype can be driven in initial/always blocks
-      		$display ("my_data=0x%0b en=%0b", new_data, en);    
+		$display ("new_data=0x%0b en=%0b", new_data, en);    
       		#1;
-      		$display ("my_data=0x%0b en=%0b", new_data, en);
+		$display ("new_data=0x%0b en=%0b", new_data, en);
   	end
  
   assign en = new_data[0];                 // logic datatype can also be driven via assign statements
@@ -17,3 +17,8 @@ endmodule
 // The same can't be said about verilog code.
 // There 'reg' can be used in procedural block and 'wire' can be used in assign statement.
 // Logic data can be of 1-bit or upwards.
+/* OUTPUT */
+// # new_data=0xx en=x
+// # new_data=0x1011 en=x
+// # new_data=0x1011 en=1 // The delay of 1 clock cycle results the en variable to be 1. otherwise it would have been 'x'. Because
+// all the commands in the initial block executes at once.
